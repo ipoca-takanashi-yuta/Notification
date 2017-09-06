bf0a2d74ddd57a6242e70944d05bdf70d356d0bb
yuta-takanashi <takanashi.y@ipoca.jp>
2017-09-06 19:10:32 +0900

    ポップアップ通知時に電源をオンにするように

--- app/src/main/java/jp/ipoca/notification/Notifier.kt
+++ app/src/main/java/jp/ipoca/notification/Notifier.kt
@@ -1,5 +1,6 @@
 package jp.ipoca.notification
 
+import android.app.KeyguardManager.KeyguardLock
 import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -7,9 +8,15 @@ import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.PowerManager
+import android.os.PowerManager.WakeLock
 import android.support.v4.app.NotificationCompat
 
+
 class Notifier : BroadcastReceiver() {
+    private var wakelock: WakeLock? = null
+    private var keylock: KeyguardLock? = null
+
     override fun onReceive(context: Context?, p1: Intent?) {
         // ただ通知するだけ
         val intent = Intent(context, MainActivity::class.java)
@@ -25,6 +32,11 @@ class Notifier : BroadcastReceiver() {
                 .setAutoCancel(true)
         val manager = context?.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         manager.notify(MainActivity.NOTIFICATION_ID, builder.build())
+
+        val wakelock = (context.getSystemService(Context.POWER_SERVICE) as PowerManager)
+                .newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.ON_AFTER_RELEASE, "disableLock")
+        wakelock.acquire(5000)
+        wakelock.release()      // リリースのタイミングはまだしっかりわかっていない
 
         // 同時にPopUpDialogの表示もする
         val intent2 = Intent(context, PopUpActivity::class.java).apply {
